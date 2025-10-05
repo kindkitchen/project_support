922f22cb04033e1ae1e78e21e00b2a18e0e49227
nik-kita <nik@kita.github>
2025-10-05 02:30:19 +0200

    feat: mvp - all md to llm.txt [/project_support/]

---
+++ README.md
@@ -0,0 +1,7 @@
+# About
+
+This is toolbox that may be handy to any project to improve developer
+experience. The simple example of code, that and for that should be placed here
+is parsing some project's markdown files (as documentation) and generate single
+`llm.txt` file. Nothing directly related to project, only helpers to support
+it's development.
---
+++ src/lib/llm_txt_from_all_md.ts
@@ -0,0 +1,62 @@
+import * as std_cli from "@std/cli";
+import * as std_fs from "@std/fs";
+import * as std_path from "@std/path";
+
+export async function llm_txt_from_all_md(options: {
+  root: string;
+}) {
+  const {
+    root,
+  } = options;
+  const llm_txt_path = std_path.join(root, "llm.txt");
+  const llm_chunks = [];
+
+  for await (
+    const {
+      path,
+    } of std_fs.walk(root, {
+      includeDirs: false,
+      exts: [".md"],
+      followSymlinks: true,
+      includeSymlinks: true,
+      maxDepth: 10,
+      skip: [/node_modules/],
+    })
+  ) {
+    const content = await Deno.readTextFile(path);
+    const deep = path.split("/").length;
+    const link = std_path.relative(root, path);
+    llm_chunks.push({ content, deep, link });
+  }
+
+  llm_chunks.sort((a, b) => {
+    if (a.deep === b.deep) {
+      return a.link.localeCompare(b.link, undefined, { caseFirst: "upper" });
+    } else {
+      return a.deep - b.deep;
+    }
+  });
+
+  await Deno.remove(llm_txt_path, { recursive: true });
+  await std_fs.ensureFile(llm_txt_path);
+
+  for (const chunk of llm_chunks) {
+    await Deno.writeTextFile(
+      llm_txt_path,
+      `> [source](${chunk.link})\n\n` +
+        chunk.content +
+        `\n\n---\n---\n---\n\n\n\n`,
+      {
+        append: true,
+      },
+    );
+  }
+}
+
+if (import.meta.main) {
+  const {
+    root = Deno.cwd(),
+  } = std_cli.parseArgs(Deno.args, { string: ["root"] });
+
+  await llm_txt_from_all_md({ root });
+}
